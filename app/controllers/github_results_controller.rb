169c84d2dc1422a28507daa6eb064ac584cbfc81
James Dindi <dindijjames@gmail.com>
2021-10-26 13:04:46 +0300

    Implement functionality with tests

--- app/controllers/github_results_controller.rb
+++ app/controllers/github_results_controller.rb
@@ -8,11 +8,30 @@ class GithubResultsController < ApplicationController
       flash[:alert] = 'Enter search text'
       return render action: :index
     end
-    @repositories = get_repositories("https://api.github.com/search/repositories?q=#{repository_params[:repositories]}")
-    if @repositories['total_count'] == 0
+    # @total_count = 0
+    # current_page = 1
+    # @repos = []
+    # while @total_count < 1000
+    #   result = get_repositories("https://api.github.com/search/repositories?q=#{repository_params[:repositories]}&per_page=100&page=#{current_page}")
+    #   page_count = result['items'].length
+    #   @total_count += result['items'].length
+    #   @repos += result['items']
+    #   current_page += 1
+    #   break if page_count < 100
+    # end
+
+    @loading = true
+    repositories = get_repositories("https://api.github.com/search/repositories?q=#{repository_params[:repositories]}&per_page=100")
+    @loading = false
+    @repos = repositories['items']
+    @total_count = @repos.length
+
+    if @total_count == 0
       flash[:alert] = 'Search returned 0 results'
       return render action: :index
     end
+
+
     render action: :index
   end
 
@@ -26,6 +45,7 @@ class GithubResultsController < ApplicationController
     response = Rails.cache.fetch(url, :expires => 1.hour) do
       HTTParty.get(url)
     end
+    # byebug
     JSON.parse(response.body)
   end
 end
